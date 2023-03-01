3b1b8027dba2c06ef8d9846467582bb19a1edae8
Maxime Van Eygen <75274590+Daarunia@users.noreply.github.com>
2023-03-01 23:32:41 +0100

    Cleanning

--- src/hooks/ChampionsData.ts
+++ src/hooks/ChampionsData.ts
@@ -1,5 +1,4 @@
 import { useQuery } from '@tanstack/react-query';
-import React, { useState, useEffect } from "react";
 
 async function fetchData() {
   const result = await fetch(`https://ddragon.leagueoflegends.com/cdn/13.1.1/data/fr_FR/champion.json`);
